b90545ede62fc3231b4250f8ab29a14e2c1fda04
kanishka-ka <kanishka.ka2005@gmail.com>
2026-05-30 12:39:30 +0530

    chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,6 @@
 from tkinter import *
 import nltk
-
-# Download required NLTK data
 nltk.download('punkt')
-
 def send_message():
     user_message = entry_box.get().lower()
 
@@ -36,14 +33,10 @@ def send_message():
     chat_box.insert(END, "Bot: " + bot_reply + "\n\n")
 
     entry_box.delete(0, END)
-
-# Main Window
 root = Tk()
 root.title("AI ChatBot")
 root.geometry("500x500")
 root.config(bg="lightblue")
-
-# Heading
 heading = Label(
     root,
     text="AI ChatBot",
@@ -51,21 +44,13 @@ heading = Label(
     bg="lightblue"
 )
 heading.pack(pady=10)
-
-# Chat Area
 chat_box = Text(root, height=20, width=50)
 chat_box.pack(pady=10)
-
-# Scrollbar
 scrollbar = Scrollbar(root, command=chat_box.yview)
 chat_box.config(yscrollcommand=scrollbar.set)
 scrollbar.pack(side=RIGHT, fill=Y)
-
-# Input Box
 entry_box = Entry(root, width=40, font=("Arial", 12))
 entry_box.pack(pady=10)
-
-# Send Button
 send_button = Button(
     root,
     text="Send",
